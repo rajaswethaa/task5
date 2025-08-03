266bae351d19c45461df6dd0e6011ae4951b46c2
rajaswethaa <earlaswetha93@gmail.com>
2025-08-03 11:17:26 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+date_default_timezone_set('Asia/Kolkata');
+$con=mysqli_connect("127.0.0.1","root","","rtbsdb");
+if(mysqli_connect_errno()){
+echo "Connection Fail".mysqli_connect_error();
+}
+
+  ?>
